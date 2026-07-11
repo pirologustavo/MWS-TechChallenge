19296df563d1d195ecf374ac7af778ebd447ab56
Gustavo Pirolo <gusto.pirolo25@hotmail.com>
2026-07-11 00:59:03 -0300

    Fix: Correção git action

--- Microservicos-lv/php84/Funcionario-lv/tests/Feature/FuncionarioControllerTest.php
+++ Microservicos-lv/php84/Funcionario-lv/tests/Feature/FuncionarioControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\Funcionario;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Laravel\Sanctum\Sanctum;
 
 class FuncionarioControllerTest extends TestCase
 {
@@ -13,19 +14,23 @@ class FuncionarioControllerTest extends TestCase
     /** @test */
     public function test_deve_listar_todos_os_funcionarios_com_campos_especificos()
     {
-        Funcionario::create([
+        $funcionario = Funcionario::create([
             'nome' => 'Gustavo Pirolo',
             'cargo' => 'Desenvolvedor',
             'usr' => 'gustavo.pirolo',
             'password' => bcrypt('123')
         ]);
 
-        Funcionario::create([
+        Sanctum::actingAs($funcionario);
+
+        $funcionario = Funcionario::create([
             'nome' => 'Erikson',
             'cargo' => 'Gerente',
-            'usr' => 'erikson.pai',
+            'usr' => 'erikson',
             'password' => bcrypt('123')
         ]);
+
+        Sanctum::actingAs($funcionario);
 
         $response = $this->getJson('/api/funcionario');
 
@@ -40,19 +45,23 @@ class FuncionarioControllerTest extends TestCase
     /** @test */
     public function test_deve_buscar_apenas_funcionarios_com_cargo_mecanico()
     {
-        Funcionario::create([
+        $funcionario = Funcionario::create([
             'nome' => 'Claudio Mecanico',
             'cargo' => 'Mecânico',
             'usr' => 'claudio.m',
             'password' => 'secret'
         ]);
 
-        Funcionario::create([
+        Sanctum::actingAs($funcionario);
+
+        $funcionario = Funcionario::create([
             'nome' => 'Claudio Atendente',
             'cargo' => 'Atendente',
             'usr' => 'claudio.a',
             'password' => 'secret'
         ]);
+
+        Sanctum::actingAs($funcionario);
 
         $response = $this->getJson('/api/funcionario/buscarMecanico?q=Claudio');
 
@@ -64,12 +73,14 @@ class FuncionarioControllerTest extends TestCase
     /** @test */
     public function test_deve_retornar_lista_vazia_se_mecanico_nao_for_encontrado()
     {
-        Funcionario::create([
+        $funcionario = Funcionario::create([
             'nome' => 'Joao',
             'cargo' => 'Mecânico',
             'usr' => 'joao.m',
             'password' => 'secret'
         ]);
+
+        Sanctum::actingAs($funcionario);
 
         $response = $this->getJson('/api/funcionario/buscarMecanico?q=Inexistente');
 
